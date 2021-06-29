27a43226fbdd4fd7f20a93fce7936535898ec838
wucanlong <wucanlong@bell.ai>
2021-06-29 16:32:57 +0800

    update

--- script/utils.ts
+++ script/utils.ts
@@ -41,10 +41,39 @@ export function createId() {
  * @param img
  * @param callback
  */
-function imageFile2Base64(img): Promise<string | ArrayBuffer | null> {
+function file2Base64(img): Promise<string> {
   const reader = new FileReader();
   return new Promise((resolve) => {
-    reader.addEventListener('load', () => resolve(reader.result));
+    reader.addEventListener('load', () => resolve(reader.result as string));
     reader.readAsDataURL(img);
   });
+}
+/**
+ * 从File对象读取加载，返回Buffer
+ * @param img
+ * @param callback
+ */
+function file2ArrayBuffer(file): Promise<ArrayBuffer> {
+  const reader = new FileReader();
+  return new Promise((resolve) => {
+    reader.addEventListener('load', () => resolve(reader.result as ArrayBuffer));
+    reader.readAsArrayBuffer(file);
+  });
+}
+/**
+	从阿里云的oss对象连接下载
+*/
+function downloadFileByOssURL(url,fileName) {
+  fetch(url)
+    .then(res => res.blob())
+    .then(myBlob => {
+      const objectURL = URL.createObjectURL(myBlob);
+      const a = document.createElement("a");
+      a.href = objectURL;
+      a.download = fileName;
+      document.body.appendChild(a);
+      a.click();
+      a.remove();
+      window.URL.revokeObjectURL(objectURL);
+    })
 }
